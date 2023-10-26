328bebc1ef3d4f49c84c51c1835d8f369d1b2a9e
Hendrik <burgdorfhendrik2@gmail.com>
2023-10-26 23:57:44 +0200

    First commit

--- Main.java
+++ Main.java
@@ -1,7 +1,16 @@
+import java.util.ArrayList;
 
 
 public class Main{
     public static void main(String [] args){
-        System.out.println("Hello world");
-    }
+      ArrayList<String> animals = new ArrayList<String> ();
+
+      animals.add("cat");
+      animals.add("dog");
+      animals.add("lion");
+
+      for(String i: animals){
+        System.out.println(i);
+      }
+       }
 }
